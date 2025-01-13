70caf654bfa6a7e3588d6a56518ba5f5b4d64a3e
shahwaj <shahwaj1221@gmail.com>
2025-01-13 17:03:30 +0530

    v1.12

--- script.js
+++ script.js
@@ -1,242 +1,212 @@
 
-const courses = [
-  {
-    name: "GWD",
-    duration: "Short",
-    courseCode: "GWDD123",
-    terms: "1 Year",
-    software: "Adobe Photoshop",
-    totalFees: "300",
-    registrationFees: "50",
-    downPayment: "100",
-    lumpSum: "250",
-    quarterly: "60",
-    totalSum: "360",
-    monthlyInstallments: "30",
-    installments: "6",
-    months: "6 months",
-    totalSubmission: "360",
-  },
-  {
-    name: "Graphic design",
-    duration: "Short",
-    courseCode: "FIF123",
-    terms: "1 Year",
-    software: "Figma",
-    totalFees: "500",
-    registrationFees: "100",
-    downPayment: "150",
-    lumpSum: "400",
-    quarterly: "100",
-    totalSum: "600",
-    monthlyInstallments: "50",
-    installments: "6",
-    months: "6 months",
-    totalSubmission: "600",
-  },
-  {
-    name: "Motion Graphic",
-    duration: "Short",
-    courseCode: "FIF123",
-    terms: "1 Year",
-    software: "Figma",
-    totalFees: "500",
-    registrationFees: "100",
-    downPayment: "150",
-    lumpSum: "400",
-    quarterly: "100",
-    totalSum: "600",
-    monthlyInstallments: "50",
-    installments: "6",
-    months: "6 months",
-    totalSubmission: "600",
-  },
-  {
-    name: "2D",
-    duration: "Mid",
-    courseCode: "WDM123",
-    terms: "2 Years",
-    software: "HTML",
-    totalFees: "1000",
-    registrationFees: "150",
-    downPayment: "200",
-    lumpSum: "800",
-    quarterly: "250",
-    totalSum: "1500",
-    monthlyInstallments: "100",
-    installments: "12",
-    months: "12 months",
-    totalSubmission: "1200",
-  },
+      const courses = [
+        {
+          name: "GWDD",
+          duration: "18 months",
+          interest: "Design",
+          courseCode: "GWDD123",
+          terms: "3",
+          totalFees: "300",
+          registrationFees: "50",
+          downPayment: "100",
+          lumpSum: "250",
+          quarterly: "60",
+          totalSum: "360",
+          monthlyInstallments: "30",
+          installments: "6",
+          months: 18,
+          totalSubmission: "360",
+          term1: "Photoshop, illustrator, Photoshop, illustrator,Photoshop, illustrator,Photoshop, illustrator",
+          term2: "Photoshop, illustrator, Photoshop, illustrator,Photoshop, illustrator,Photoshop, illustrator",
+          term3: "Photoshop, illustrator, Photoshop, illustrator,Photoshop, illustrator,Photoshop, illustrator",
+        },
+        {
+          name: "UI/UX",
+          duration: "12 months",
+          interest: "UI/UX",
+          courseCode: "FIF123",
+          terms: "1",
+          totalFees: "500",
+          registrationFees: "100",
+          downPayment: "150",
+          lumpSum: "400",
+          quarterly: "100",
+          totalSum: "600",
+          monthlyInstallments: "50",
+          installments: "6",
+          months: 12,
+          totalSubmission: "600",
+        },
+        {
+          name: "3D",
+          duration: "Mid",
+          interest: "Development",
+          courseCode: "WDM123",
+          terms:"3",
+          totalFees: "1000",
+          registrationFees: "150",
+          downPayment: "200",
+          lumpSum: "800",
+          quarterly: "250",
+          totalSum: "1500",
+          monthlyInstallments: "100",
+          installments: "12",
+          months: 18,
+          totalSubmission: "1200",
+          term1: "Photoshop, illustrator",
+          term2: "Photoshop, illustrator",
+          term3: "Photoshop, illustrator",
+        },
+        {
+          name: "Graphic Designing",
+          duration: "Long",
+          interest: "AI",
+          courseCode: "DSA123",
+          terms:  "3",
+          totalFees: "3000",
+          registrationFees: "200",
+          downPayment: "500",
+          lumpSum: "2000",
+          quarterly: "500",
+          totalSum: "3000",
+          monthlyInstallments: "250",
+          installments: "12",
+          months: 36,
+          totalSubmission: "3000",
+        },
+        {
+          name: "Advanced Graphic Design",
+          duration: "Mid",
+          interest: "Design",
+          courseCode: "AGD456",
+          terms: "3",
+          totalFees: "1200",
+          registrationFees: "150",
+          downPayment: "300",
+          lumpSum: "900",
+          quarterly: "300",
+          totalSum: "1800",
+          monthlyInstallments: "150",
+          installments: "12",
+          months: 18,
+          totalSubmission: "1800",
+          term1: "Photoshop, illustrator",
+          term2: "Photoshop, illustrator",
+          term3: "Photoshop, illustrator",
+        },
+      ];
 
-{
-    name: "3D",
-    duration: "Mid",
-    courseCode: "WDM123",
-    terms: "2 Years",
-    software: "HTML",
-    totalFees: "1000",
-    registrationFees: "150",
-    downPayment: "200",
-    lumpSum: "800",
-    quarterly: "250",
-    totalSum: "1500",
-    monthlyInstallments: "100",
-    installments: "12",
-    months: "12 months",
-    totalSubmission: "1200",
-  },
-{
-    name: "GWDD",
-    duration: "Mid",
-    courseCode: "WDM123",
-    terms: "1.5 Years",
-    software: "HTML",
-    totalFees: "1000",
-    registrationFees: "150",
-    downPayment: "200",
-    lumpSum: "800",
-    quarterly: "250",
-    totalSum: "1500",
-    monthlyInstallments: "100",
-    installments: "12",
-    months: "12 months",
-    totalSubmission: "1200",
-  },
-  {
-    name: "B.Voc",
-    duration: "Long",
-    courseCode: "DSA123",
-    terms: "3 Years",
-    software: "Photoshop, Illustrator",
-    totalFees: "3000",
-    registrationFees: "200",
-    downPayment: "500",
-    lumpSum: "2000",
-    quarterly: "500",
-    totalSum: "3000",
-    monthlyInstallments: "250",
-    installments: "12",
-    months: "12 months",
-    totalSubmission: "3000",
-  },
-  {
-    name: "VFX film Making",
-    duration: "Long",
-    courseCode: "AGD456",
-    terms: "2 Years",
-    software: "Adobe Illustrator",
-    totalFees: "1200",
-    registrationFees: "150",
-    downPayment: "300",
-    lumpSum: "900",
-    quarterly: "300",
-    totalSum: "1800",
-    monthlyInstallments: "150",
-    installments: "12",
-    months: "12 months",
-    totalSubmission: "1800",
-  },
-  {
-    name: "M.Voc",
-    duration: "Long",
-    courseCode: "WDM123",
-    terms: "2 Years",
-    software: "HTML",
-    totalFees: "1000",
-    registrationFees: "150",
-    downPayment: "200",
-    lumpSum: "800",
-    quarterly: "250",
-    totalSum: "1500",
-    monthlyInstallments: "100",
-    installments: "12",
-    months: "12 months",
-    totalSubmission: "1200",
-  },
-];
+      let selectedCourse = null;
 
-let selectedCourse = null;
+      function searchByInterest() {
+        document.getElementById("course-grid").innerHTML = "";
+        document.getElementById("result-text").style.display = "none";
 
-function searchByInterest() {
-  const interestInput = document.getElementById("interest").value.toLowerCase();
-  if (interestInput.length === 0) {
-    document.getElementById("course-grid").innerHTML = "";
-    document.getElementById("result-text").style.display = "none";
-    return;
-  }
+        document.getElementById("duration").value = "";
 
-  const filteredCourses = courses.filter((course) =>
-    course.name.toLowerCase().includes(interestInput)
-  );
-  displayCourses(filteredCourses);
+        const interestInput = document.getElementById("interest").value.toLowerCase();
+        if (interestInput.length === 0) {
+          return;
+        }
 
-  const resultText = document.getElementById("result-text");
-  resultText.style.display = filteredCourses.length > 0 ? "block" : "none";
-}
+        const filteredCourses = courses.filter((course) =>
+          course.name.toLowerCase().includes(interestInput)
+        );
+        displayCourses(filteredCourses);
 
-function searchByFilter() {
-  const selectedDuration = document.getElementById("duration").value;
-  const filteredCourses = courses.filter(
-    (course) => course.duration === selectedDuration
-  );
-  displayCourses(filteredCourses);
+        const resultText = document.getElementById("result-text");
+        resultText.style.display = filteredCourses.length > 0 ? "block" : "none";
+      }
 
-  const resultText = document.getElementById("result-text");
-  resultText.style.display = filteredCourses.length > 0 ? "block" : "none";
-}
+      function searchByFilter() {
+        document.getElementById("interest").value = '';
+        document.getElementById("course-grid").innerHTML = "";
+        document.getElementById("result-text").style.display = "none";
 
-function displayCourses(filteredCourses) {
-  const courseGrid = document.getElementById("course-grid");
-  courseGrid.innerHTML = "";
+        const selectedDuration = document.getElementById("duration").value;
+        let filteredCourses = [];
 
-  filteredCourses.forEach((course) => {
-    const courseCard = document.createElement("div");
-    courseCard.className = "course-card";
-    courseCard.innerHTML = `
-      <h4 onclick="showCourseDetail('${course.name}')">
-       ${course.name} <i class="fa-solid fa-right-long fa-beat"></i>
-      </h4>
-    `;
-    courseGrid.appendChild(courseCard);
-  });
-}
+        if (selectedDuration === "Short") {
+          filteredCourses = courses.filter(
+            (course) => course.months <= 12
+          );
+        } else if (selectedDuration === "Mid") {
+          filteredCourses = courses.filter(
+            (course) => course.months <= 18
+          );
+        } else if (selectedDuration === "Long") {
+          filteredCourses = courses.filter(
+            (course) => course.months > 18
+          );
+        }
 
-function showCourseDetail(courseName) {
-  selectedCourse = courses.find((course) => course.name === courseName);
-  document.getElementById("course-detail-title").textContent = selectedCourse.name;
+        displayCourses(filteredCourses);
 
-  document.getElementById("course-detail-table1").innerHTML = `
-    <td>${selectedCourse.courseCode}</td>
-    <td>${selectedCourse.name}</td>
-    <td>${selectedCourse.duration}</td>
-    <td>${selectedCourse.terms}</td>
-    <td>${selectedCourse.software}</td>
-  `;
+        const resultText = document.getElementById("result-text");
+        resultText.style.display = filteredCourses.length > 0 ? "block" : "none";
+      }
 
-  document.getElementById("course-detail-table2").innerHTML = `
-    <td>${selectedCourse.totalFees}</td>
-    <td>${selectedCourse.registrationFees}</td>
-    <td>${selectedCourse.downPayment}</td>
-  `;
+      function displayCourses(filteredCourses) {
+        const courseGrid = document.getElementById("course-grid");
+        courseGrid.innerHTML = "";
 
-  document.getElementById("course-detail-table3").innerHTML = `
-    <td>${selectedCourse.lumpSum}</td>
-    <td>${selectedCourse.quarterly}</td>
-    <td>${selectedCourse.totalSum}</td>
-    <td>${selectedCourse.monthlyInstallments}</td>
-  `;
+        filteredCourses.forEach((course) => {
+          const courseCard = document.createElement("div");
+          courseCard.className = "course-card";
+          courseCard.innerHTML = `
+            <h4 onclick="showCourseDetail('${course.name}')">
+             ${course.name} <i class="fa-solid fa-right-long fa-beat"></i>
+            </h4>
+          `;
+          courseGrid.appendChild(courseCard);
+        });
+      }
 
-  document.getElementById("course-detail-table4").innerHTML = `
-    <td>${selectedCourse.installments}</td>
-    <td>${selectedCourse.months}</td>
-    <td>${selectedCourse.totalSubmission}</td>
-  `;
+      function showCourseDetail(courseName) {
+        selectedCourse = courses.find((course) => course.name === courseName);
+        document.getElementById("course-detail-title").textContent = selectedCourse.name;
 
-  document.getElementById("course-detail-section").style.display = "block";
-}
+        document.getElementById("course-detail-table1").innerHTML = `
+          <td>${selectedCourse.courseCode}</td>
+          <td>${selectedCourse.name}</td>
+          <td>${selectedCourse.duration}</td>
+          <td>${selectedCourse.terms}</td>
+        `;
 
-function goBack() {
-  document.getElementById("course-detail-section").style.display = "none";
-}
+        // Show or hide table with id="course-detail-table5" based on availability of terms
+        if (selectedCourse.term1 && selectedCourse.term2 && selectedCourse.term3) {
+          document.getElementById("course-detail-table5").style.display = "block";
+          document.getElementById("course-detail-table5-content").innerHTML = `
+            <td>${selectedCourse.term1}</td>
+            <td>${selectedCourse.term2}</td>
+            <td>${selectedCourse.term3}</td>
+          `;
+        } else {
+          document.getElementById("course-detail-table5").style.display = "none";
+        }
 
+        document.getElementById("course-detail-table2").innerHTML = `
+          <td>${selectedCourse.totalFees}</td>
+          <td>${selectedCourse.registrationFees}</td>
+          <td>${selectedCourse.downPayment}</td>
+        `;
+
+        document.getElementById("course-detail-table3").innerHTML = `
+          <td>${selectedCourse.lumpSum}</td>
+          <td>${selectedCourse.quarterly}</td>
+          <td>${selectedCourse.totalSum}</td>
+          <td>${selectedCourse.monthlyInstallments}</td>
+        `;
+
+        document.getElementById("course-detail-table4").innerHTML = `
+          <td>${selectedCourse.installments}</td>
+          <td>${selectedCourse.months}</td>
+          <td>${selectedCourse.totalSubmission}</td>
+        `;
+
+        document.getElementById("course-detail-section").style.display = "block";
+      }
+
+      function goBack() {
+        document.getElementById("course-detail-section").style.display = "none";
+      }
